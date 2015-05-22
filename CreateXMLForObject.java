824115512cac00e57e6c055b37d9c9fa4e80e7b3
Ishan Randeniya <time4ish@gmail.com>
2015-05-22 14:12:00 +0530

    Create CreateXMLForObject.java

---
+++ CreateXMLForObject.java
@@ -0,0 +1,34 @@
+//Download JOParser2.0 from here https://goo.gl/4SFCpR
+
+
+import DB.Customer;
+import jop.Parser;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Ishan
+ */
+public class CreateXMLForObject {
+
+    public static void main(String[] args) {
+        try {
+            Customer customer = new Customer();
+            customer.setFirstname("ABCD");
+            customer.setLastname("QWER");
+            customer.setIdCustomer(100);
+            String xml = Parser.getParser().createXMLForObject(customer);
+            System.out.println(xml);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+//output
+//<object class="DB.Customer"><param><name>idCustomer</name><object class="java.lang.Integer">100</object></param><param><name>firstname</name><object class="java.lang.String">ABCD</object></param><param><name>lastname</name><object class="java.lang.String">QWER</object></param></object>
+
